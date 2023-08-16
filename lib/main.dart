f769b7b3914ff696e43019f505fe3fe683bd07ef
Boris Sea <seaborisrc@gmail.com>
2023-08-16 16:34:54 -0400

    Add my first Scafold to the Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: Center( child: Text('Flutter BorisXD')),
-    );
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(child: Text('Flutter BorisXD')),
+        ));
   }
 }
