56b39b5fd3a8cceb41abf00cfd11db3097133bef
Boris Sea <seaborisrc@gmail.com>
2023-08-17 23:45:44 -0400

    Add Material 3

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: 'Flutter Demo',
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: CounterScreen());
+        theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: Color.fromARGB(255, 4, 100, 255)
+        ),
+        home: const CounterScreen());
   }
 }
